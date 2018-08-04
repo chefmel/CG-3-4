b1fd7932887a9828a9baa66b01a1c861f0c08d20
Melissa DoddRuesing <melissa.doddruesing@gmail.com>
2018-08-04 10:26:03 -0500

    calculator

--- CG 3-4/Program.cs
+++ CG 3-4/Program.cs
@@ -6,7 +6,34 @@ namespace CG_3_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter 2 numbers");
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Please choose +, -, X or /");
+            string op = Console.ReadLine();
+            if (op == "+")
+            {
+                Console.WriteLine(x + y); 
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine(x - y);
+            }
+            else if (op== "x")
+            {
+                Console.WriteLine(x * y);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine(x / y);
+            }
+            else { Console.WriteLine("you did not follow directions"); }
+            Console.ReadLine();
+
+
+
         }
     }
 }
